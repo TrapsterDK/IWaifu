64859baea34fcb62899f900bea81da7aedddc7c6
TrapsterDK <martin.kuntz.jacobsen@gmail.com>
2023-03-19 20:40:18 +0100

    small fixes

--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
 
 import hashlib
 import os
+import pathlib
 
 def generate_salt() -> str:
     return os.urandom(32).hex()
 
 def hash_password(plaintext: str, salt: str) -> str:
     return hashlib.pbkdf2_hmac('sha256', plaintext, salt, 10000)
+
