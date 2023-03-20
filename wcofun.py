0f1939ebf59a035f8100d6a077e22a6436dc4c82
TrapsterDK <martin.kuntz.jacobsen@gmail.com>
2023-03-20 16:00:32 +0100

    a

--- wcofun.py
+++ wcofun.py
@@ -5,6 +5,12 @@ import js2py
 import re
 from urllib.parse import urlsplit
 from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from time import sleep
+from copy import copy
+from threading import Lock
+
+js2py_lock = Lock()
 
 URL = "https://www.wcofun.com/"
 ANIME_URL = URL + "anime/"
@@ -22,7 +28,6 @@ def get_episodes(anime, scraper):
         episodes.append((episode.text, episode["href"]))
 
     return episodes
-
 
 
 def atob(s):
@@ -47,8 +52,10 @@ def get_video_url(episode_url, scraper):
     script = iframe_script.text.replace("document.write", "")
 
     ctx = js2py.EvalJs({'atob': atob})
+    sleep(1)
 
-    iframe_soup = BeautifulSoup(ctx.eval(script), "html.parser")
+    with js2py_lock:
+        iframe_soup = BeautifulSoup(ctx.eval(copy(str(script))), "html.parser")
 
     iframe = iframe_soup.select_one("iframe")
     if iframe is None:
@@ -72,7 +79,6 @@ def get_video_url(episode_url, scraper):
     
     scraper.headers.update({"x-requested-with": "XMLHttpRequest"})
     scraper.headers.update({"Referer": iframe_url})
-    scraper.headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"})
 
     url = urlsplit(iframe_url)
     video_get_url = url.scheme + "://" + url.netloc + video_get_url
@@ -86,45 +92,59 @@ def get_video_url(episode_url, scraper):
     if not ("server" in video_json and "enc" in video_json): 
         raise Exception("Could not find video url in json")
     
-    scraper.headers.pop("x-requested-with")
-
     return video_json["server"] + "/getvid?evid=" + video_json["enc"]
 
-episodes = None
 
-scraper = cloudscraper.create_scraper()
+def scraper_retry(function, *args, max_retries=15, **kwargs):
+    scraper = cloudscraper.create_scraper()
 
-"""
+    tries = 0
+    while True:
+        try:
+            tries += 1
+            if tries > max_retries:
+                return None
+
+            return function(*args, **kwargs, scraper=scraper)
+        except cloudscraper.exceptions.CloudflareChallengeError:
+            scraper = cloudscraper.create_scraper()
+
+
+def get_all_videos(anime):
+    with ThreadPoolExecutor() as executor:
+        episodes = scraper_retry(get_episodes, anime)
+        print(f"Found {len(episodes)} episodes, getting video urls")
+        results = list(tqdm(executor.map(scraper_retry, [get_video_url] * len(episodes), [episode[1] for episode in episodes]), total=len(episodes)))
+ 
+        return results
+
+def download_video(video_url, filename):
+    r = scraper.get(link, stream=True)
+    with open("test.mp4", "wb") as handle:
+        for data in tqdm(r.iter_content()):
+            handle.write(data)
+
+print(get_all_videos("sword-art-online"))
+
+
+'''
+scraper = cloudscraper.create_scraper() 
 while True:
     try:
-        episodes = get_episodes("sword-art-online", cloudscraper.create_scraper())
-        break
-    except Exception as e:
-        scraper = cloudscraper.create_scraper()
-        print("except")
+        #link = get_video_url("https://www.wcofun.com/sword-art-online-episode-12-english-dubbed-2", scraper) 
+        #link2 = get_video_url("https://www.wcofun.com/sword-art-online-episode-13-english-dubbed-2", scraper) 
+        #print(link)
+        #print(link2)
 
-print(episodes)
-
-for episode in episodes:
-    try:
-        print(get_video_url(episode[1], cloudscraper.create_scraper()))
-    except Exception as e:        
-        scraper = cloudscraper.create_scraper()
-        print("except")
-"""
-
-while True:
-    try:
-        link = get_video_url("https://www.wcofun.com/sword-art-online-episode-12-english-dubbed-2", scraper) 
-        print(link)
-
-        r = scraper.get(link, stream=True)
+        """r = scraper.get(link, stream=True)
         with open("test.mp4", "wb") as handle:
             for data in tqdm(r.iter_content()):
                 handle.write(data)
+        """
 
         break
 
     except Exception as e:
         scraper = cloudscraper.create_scraper()
         print(e)
+'''
