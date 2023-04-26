cdee24099f1896b64c6d08d5c30f85d5f7481c74
TrapsterDK <martin.kuntz.jacobsen@gmail.com>
2023-04-26 13:43:40 +0200

    a

--- server.py
+++ server.py
@@ -171,4 +171,5 @@ def logout():
 
 if __name__ == "__main__":
     print("Starting server...")
-    app.run("0.0.0.0", debug=True)
+    app.run("localhost", debug=True)
+    #app.run("0.0.0.0", debug=True)
